fdd2c16b5ee5ab478980d08a7072dba6ec8fd883
Ted M. Young <tedyoung@gmail.com>
2021-11-17 11:49:12 -0800

    Lab #2: Moved Hand display to ConsoleHand

--- src/main/java/com/jitterted/ebp/blackjack/Hand.java
+++ src/main/java/com/jitterted/ebp/blackjack/Hand.java
@@ -2,9 +2,6 @@ package com.jitterted.ebp.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.fusesource.jansi.Ansi.ansi;
 
 public class Hand {
     private final List<Card> cards = new ArrayList<>();
@@ -44,14 +41,12 @@ public class Hand {
     }
 
     void display() {
-        System.out.println(cardsAsString());
+        System.out.println(ConsoleHand.cardsAsString(this));
     }
 
-    public String cardsAsString() {
-        return cards.stream()
-                                .map(ConsoleCard::display)
-                                .collect(Collectors.joining(
-                                        ansi().cursorUp(6).cursorRight(1).toString()));
+    // Query: *Snapshot* of point-in-time information, shouldn't allow clients to modify state of object
+    public List<Card> cards() {
+        return List.copyOf(cards);
     }
 
     public void drawFrom(Deck deck) {
